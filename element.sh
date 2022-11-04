6b328a3bcf09eccb6e1e3a2b454482edbe883bff
nicolasmartinsanchez <nicolasmartin981@gmail.com>
2022-11-04 21:32:19 +0000

    feat: getting basic information from the argument

--- element.sh
+++ element.sh
@@ -2,3 +2,14 @@
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
 echo Please provide an element as an argument.
+
+if [[ $1 =~ ^[0-9]+$ ]]
+then
+  ELEMENT_ATOMIC_NUMBER=$($PSQL "select atomic_number from elements where atomic_number=$1")
+  ELEMENT_SYMBOL=$($PSQL "select symbol from elements where atomic_number=$1 or symbol='$1' or name='$1'")
+  ELEMENT_NAME=$($PSQL "select name from elements where atomic_number=$1 or symbol='$1' or name='$1'")
+else
+  ELEMENT_ATOMIC_NUMBER=$($PSQL "select atomic_number from elements where symbol='$1' or name='$1'")
+  ELEMENT_SYMBOL=$($PSQL "select symbol from elements where symbol='$1' or name='$1'")
+  ELEMENT_NAME=$($PSQL "select name from elements where symbol='$1' or name='$1'")
+fi
